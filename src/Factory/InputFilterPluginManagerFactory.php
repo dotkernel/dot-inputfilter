39c92f1530b34149dc5493c4dc5a256b0fad8a22
n3vrax <n3vrax@gmail.com>
2017-03-15 21:49:48 +0200

    updated factories to use PSR11 container

--- src/Factory/InputFilterPluginManagerFactory.php
+++ src/Factory/InputFilterPluginManagerFactory.php
@@ -9,7 +9,7 @@ declare(strict_types = 1);
 
 namespace Dot\InputFilter\Factory;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Zend\InputFilter\InputFilterPluginManager;
 
 /**
